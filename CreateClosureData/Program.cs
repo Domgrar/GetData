eb61d28f098a307c61b819a61934e602a2076c3d
jeremy farmer <jeremy.farmer@dhgllp.com>
2018-05-11 16:58:18 -0400

    Final

--- CreateClosureData/Program.cs
+++ CreateClosureData/Program.cs
@@ -1,9 +1,11 @@
-﻿using OpenQA.Selenium;
+﻿using CsvHelper;
+using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,17 +38,49 @@ namespace CreateClosureData
 
 
             ticketList = GetIncidentList();
-            
+
+            List<Ticket> fullList = new List<Ticket>();
             foreach(string item in ticketList)
             {
 
                 thisTicket = getAllTicketData(item, driver);
+                fullList.Add(thisTicket);
 
                 //Block to display all values for testing
                 Console.WriteLine("Requesting Person : " + thisTicket.Requestor);
                 Console.WriteLine("Category : " + thisTicket.Category);
                 Console.WriteLine("Solved by : " + thisTicket.SolvedBy);
                 Console.WriteLine("Description : " + thisTicket.Description);
+            }
+
+            //Write each ticket to csv file
+            using (var mem = new MemoryStream())
+            using (var writer = new StreamWriter(@"C:\FTG\test.csv"))
+            using (var csvWriter = new CsvWriter(writer))
+            {
+                csvWriter.Configuration.Delimiter = ",";
+                
+
+                csvWriter.WriteField("Requestor");
+                csvWriter.WriteField("Category");
+                csvWriter.WriteField("Resolved By");
+                csvWriter.NextRecord();
+
+                foreach (Ticket ticket in fullList)
+                {
+                    csvWriter.WriteField(ticket.Requestor);
+                    csvWriter.WriteField(ticket.Category);
+                    csvWriter.WriteField(ticket.SolvedBy);
+                    csvWriter.NextRecord();
+                }
+
+                writer.Flush();
+                var result = Encoding.UTF8.GetString(mem.ToArray());
+                Console.WriteLine(result);
+            }
+            foreach (Ticket ticket in fullList)
+            {
+
             }
 
             Console.ReadLine();
@@ -101,11 +135,12 @@ namespace CreateClosureData
 
           
                     tableElement = driver.FindElement(By.XPath("//table[@id='tbl_dialog_body_section_1_0']"));
-                    thisTicket.SolvedBy = tableElement.FindElement(By.XPath("//td[contains(., ',')]")).Text;
-         
+            thisTicket.SolvedBy = tableElement.FindElement(By.XPath("//td[contains(., ',')]")).Text;
+            
 
-         
-                    descriptionElement = driver.FindElement(By.XPath("//div[@id='SD_REQUEST_COMMENT1']"));
+
+
+            descriptionElement = driver.FindElement(By.XPath("//div[@id='SD_REQUEST_COMMENT1']"));
                     thisTicket.Description = descriptionElement.Text;
            
       
@@ -127,7 +162,7 @@ namespace CreateClosureData
         public static List<string> GetIncidentList()
         {
             List<string> iList = new List<string>();
-            string text = System.IO.File.ReadAllText(@"C:\Users\jf6856\source\repos\CreateClosureData\CreateClosureData\TicketCloses.txt"); // Upgrade to relative path
+            string text = System.IO.File.ReadAllText(@"c:\users\jf6856\onedrive - dhg llp\github\getdata\getdata\createclosuredata\ticketcloses.txt"); // Upgrade to relative path
 
             iList.AddRange(text.Split(' '));
  
